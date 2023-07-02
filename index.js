79bf1feececc2337ec24715b8a9bbbf217feedd7
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-07-02 09:37:05 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+
+let form = document.querySelector('form')
+form.addEventListener('submit', submitData)
+
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, config)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.getElementById('resultContainer').innerHTML = id;
+      })
+      .catch(error => {
+        document.getElementById('resultContainer').innerHTML = error.message;
+      });
+  }
+  
